25029bb17c2d9a7e285d9bb17697118f6b0fa495
luucasmorato <luucasmorato@gmail.com>
2019-10-28 21:43:26 -0300

    bug data

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.get('/blog', (req, res) => {
 
 app.post('/blog/leads', (req, res) => {
 
-    const ip = req.RemoteAddr;
+    const ip = req.headers.host;
     const { email, nome, tipo } = req.body
 
-    var data = new Date().toLocaleString("pt-BR"),
+    var data = new Date().toLocaleString("pt-BR"),    
     dia  = data.getDate().toString(),
     diaF = (dia.length == 1) ? '0'+dia : dia,
     mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
